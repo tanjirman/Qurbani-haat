895bc2438e780c4d79431ad708b997a469e64867
tanjirman <tanjirmahbub07@gmail.com>
2026-05-07 00:25:10 +0600

    Modify the base URL

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://qurbani-haat-seven.vercel.app"
+    baseURL: "https://qurbani-haat-git-main-tanjirmahbub07-6178s-projects.vercel.app"
 })
 
 
